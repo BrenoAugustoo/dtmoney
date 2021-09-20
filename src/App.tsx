6e6265d4e2cc5e822b98c3d3b4f60136d3861001
BrenoAugustoo <brenoaugusto98@gmail.com>
2021-09-20 20:23:35 -0300

    created modal / updated header and transactionstable

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+
 import { Header } from './components/Header';
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from './components/NewTransactionsModal';
+
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
+  const [ isNewTransactionsModalOpen, setisNewTransactionsModalOpen ] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setisNewTransactionsModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setisNewTransactionsModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard/>
+
+      <NewTransactionModal
+        isOpen={isNewTransactionsModalOpen}
+        onCloseNewTransactionModal={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle/>
     </>
   );
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "./styles";
 
 import { api } from '../../services/api';
 
+interface Transaction {
+  id: number,
+  title: string,
+  type: string,
+  category: string,
+  amount: number,
+  createdAt: string,
+}
+
 export function TransactionsTable () {
+
+  const [ transactions, setTransactions ] = useState<Transaction[]>([]);
+
   useEffect(() => {
     api.get('transactions')
-      .then(response => console.log(response.data));
+      .then(response => setTransactions(response.data.transactions));
   }, [])
 
   return (
@@ -22,18 +34,29 @@ export function TransactionsTable () {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className="deposit">R$ 12.000,00</td>
-            <td>Venda</td>
-            <td>13/04/2021</td>
-          </tr>  
-          <tr>
-            <td>Alguel</td>
-            <td className="withdraw" >-R$ 1.100,00</td>
-            <td>Pagamento</td>
-            <td>10/04/2021</td>
-          </tr>         
+          { transactions.map( transaction => { 
+            return (
+              <tr key={transaction.id} >
+                <td> {transaction.title } </td>
+                <td className={transaction.type}>
+                  { 
+                    new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(transaction.amount)
+                  }
+                </td>
+                <td>{transaction.category}</td>
+                <td>
+                   { 
+                    new Intl.DateTimeFormat('pt-BR').format(
+                      new Date(transaction.createdAt))
+                    }  
+                </td>
+              </tr>  
+            )
+          } ) }
+         
         </tbody>
       </table>  
     </Container>
