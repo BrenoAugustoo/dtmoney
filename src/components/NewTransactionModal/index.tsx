aba21f2d7b68111f4e30be026e5097c5d97e9e31
BrenoAugustoo <brenoaugusto98@gmail.com>
2021-09-21 20:36:51 -0300

    added transactionhook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
-import { useState, FormEvent, useContext } from 'react';
+import { useState, FormEvent } from 'react';
 import Modal from 'react-modal';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
+
 
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
@@ -19,22 +20,29 @@ interface NewTransactionModalProps {
 export function NewTransactionModal ( 
   { isOpen, onCloseNewTransactionModal  } : NewTransactionModalProps 
 ) {
-  const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions()
 
   const [title, setTitle ] = useState('');
   const [amount, setAmount ] = useState(0);
   const [category, setCategory ] = useState('');
   const [type, setType] = useState('deposit')
 
-  function handleCreateNewTransaction(event: FormEvent ) {
+  async function handleCreateNewTransaction(event: FormEvent ) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title, 
       amount,
       category,
       type
     })
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit')
+
+    onCloseNewTransactionModal();
   }
 
   
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,15 +1,29 @@
-import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
+
 
 import { Container } from './styles';
 
 export function Summary () {
-  const {transactions} = useContext(TransactionsContext);
+  const {transactions} = useTransactions();
   
-  console.log(transactions);
+  const summary = transactions.reduce(( acc, transaction ) => {
+    if ( transaction.type === 'deposit' ){
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
 
   return (
     <Container>
@@ -19,7 +33,14 @@ export function Summary () {
           <img src={incomeImg} alt="entradas"></img>
         </header>
     
-        <strong>1.000,00</strong>
+        <strong>
+        { 
+           new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.deposits)
+        }
+        </strong>
       </div>
       <div>
         <header>
@@ -27,7 +48,13 @@ export function Summary () {
           <img src={outcomeImg} alt="saídas"></img>
         </header>
     
-        <strong>-500,00</strong>
+        <strong>-{ 
+           new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.withdraws)
+            } 
+        </strong>
       </div>
       <div className="highlight-background" >
         <header>
@@ -35,7 +62,14 @@ export function Summary () {
           <img src={totalImg} alt="total"></img>
         </header>
     
-        <strong>500,00</strong>
+        <strong>
+        { 
+           new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.total)
+        }
+        </strong>
       </div>
     </Container>
   );
